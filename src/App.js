57d5a9a761e0d10d9df0e09c68d6b6b8ef94232f
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2021-11-14 13:43:36 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
+function CoffeeIngredients(props) {
+  const {currentCoffee} = props
+  const ingredients = currentCoffee.ingredients
+  return (
+    ingredients.map((ingredient, id) => {
+      return (
+        <p key={id}>{ingredient}</p>
+      )
+    })
+  )
+}
 
 function App() {
+  const [coffee, setCoffee] = useState([]);
+
+  useEffect(() => {
+    const url = "https://api.sampleapis.com/coffee/hot"
+    fetch(url)
+    .then((res) => {
+       return res.json();
+    })
+    .then((data) => {
+      setCoffee(data)
+    })
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container App">
+      <h1 className="my-3">Coffee</h1>
+      <div id="coffee">
+      {
+        coffee.map((item) => {
+          return (
+            <div key={item.id} className="card mb-3">
+            <div className="card-body">
+              <h2>{item.title}</h2>
+              <p>{item.description}</p>
+              <div id="ingredients">
+                <h4>Ingredients</h4>
+                <CoffeeIngredients currentCoffee={item}/>
+              </div>
+            </div>
+            </div>
+          )
+        })
+      }
+      </div>
     </div>
   );
 }
